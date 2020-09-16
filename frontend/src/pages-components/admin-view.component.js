8ea7d23a54d243377b1f33a76c1c6f734205a686
ZaitcevIlya <intiguan@yahoo.com>
2020-09-16 23:13:24 +0300

    blueprint of the project

---
+++ frontend/src/pages-components/admin-view.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class AdminView extends Component {
+  render() {
+    return (
+      <div className='container'>
+        Admin view
+      </div>
+    )
+  }
+}
---
+++ frontend/src/pages-components/employee-view.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class EmployeeView extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Add performance feedback for these employees</h3>
+      </div>
+    )
+  }
+}
